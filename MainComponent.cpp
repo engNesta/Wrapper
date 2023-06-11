62a6990f135490dc821164a408b00f16196c4080
engNesta <robert.nuhu0002@stud.hkr.se>
2023-06-11 19:36:21 +0200

    File Chooser Implemented

--- MainComponent.cpp
+++ MainComponent.cpp
@@ -1,9 +1,14 @@
 #include "MainComponent.h"
+//include vst3 headers
+#include <juce_audio_plugin_client/juce_audio_plugin_client_VST3.cpp>
 
 //==============================================================================
 MainComponent::MainComponent()
 {
     setSize (600, 400);
+    loadButton.setButtonText("Load VST3");
+    loadButton.addListener(this);
+    addAndMakeVisible(loadButton);
 }
 
 MainComponent::~MainComponent()
@@ -18,7 +23,6 @@ void MainComponent::paint (juce::Graphics& g)
 
     g.setFont (juce::Font (16.0f));
     g.setColour (juce::Colours::white);
-    g.drawText ("Wrapper, added VST3 headers!", getLocalBounds(), juce::Justification::centred, true);
 }
 
 void MainComponent::resized()
@@ -26,4 +30,46 @@ void MainComponent::resized()
     // This is called when the MainComponent is resized.
     // If you add any child components, this is where you should
     // update their positions.
+    loadButton.setBounds(getLocalBounds().reduced(20));
+}
+
+void MainComponent::initialiseVST3Hosting() {
+    //
+
+}
+
+void MainComponent::loadVST3Plugin() {
+    chooser = std::make_unique<juce::FileChooser>("Select a VST3 plugin to load",
+                                                  juce::File::getSpecialLocation(juce::File::userHomeDirectory),
+                                                  "*.vst3");
+
+    auto fileFlags = juce::FileBrowserComponent::canSelectFiles | juce::FileBrowserComponent::openMode;
+
+
+    chooser ->launchAsync(fileFlags, [this] (const FileChooser& chooser)
+    {
+        File vst3File = chooser.getResult();
+
+        if(vst3File.hasFileExtension(".vst3"))
+        {
+            loadButton.setButtonText("Correct File");
+        }
+        else
+        {
+            loadButton.setButtonText("Incorrect File");
+        }
+
+
+    });
+
+
+}
+
+void MainComponent::buttonClicked(juce::Button *button) {
+    if (button == &loadButton) {
+
+        button->setButtonText("Loading...");
+
+        loadVST3Plugin();
+    }
 }
--- MainComponent.h
+++ MainComponent.h
@@ -1,7 +1,6 @@
 #pragma once
 
 #include <JuceHeader.h>
-//include the vst3 sdk headers
 #include <juce_audio_plugin_client/juce_audio_plugin_client_VST3.cpp>
 
 //==============================================================================
@@ -9,7 +8,7 @@
     This component lives inside our window, and this is where you should put all
     your controls and content.
 */
-class MainComponent  : public juce::Component
+class MainComponent  : public juce::Component, public juce::Button::Listener
 {
 public:
     //==============================================================================
@@ -19,10 +18,16 @@ public:
     //==============================================================================
     void paint (juce::Graphics&) override;
     void resized() override;
+    void buttonClicked(juce::Button* button) override;
+    void initialiseVST3Hosting();
+    void loadVST3Plugin();
+
 
 private:
     //==============================================================================
     // Your private member variables go here...
+    juce::TextButton loadButton;
+    std::unique_ptr<juce::FileChooser> chooser;
 
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MainComponent)
